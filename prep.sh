532ed32d89f5e3cd7fc2eb1f1656f792530f3fc6
Nick Lanam <nick.lanam@tekraq.com>
2020-12-07 22:34:27 -0800

    More robust template script for aliasing

--- prep.sh
+++ prep.sh
@@ -3,10 +3,11 @@ if [ -z "${1}" ]; then
   echo "Please provide day number as an argument"
   exit 1
 fi
+echo $0
 curl "https://adventofcode.com/2020/day/${1}/input" \
- -H "Cookie: session=$(cat "$(dirname $0)/input/cookie.txt")" \
- -o "$(dirname $0)/input/day${1}.txt"
-cat > "$(dirname $0)/day${1}.js" <<EOF
+ -H "Cookie: session=$(cat "$(dirname "$0")/input/cookie.txt")" \
+ -o "$(dirname "$0")/input/day${1}.txt"
+cat > "$(dirname "$0")/day${1}.js" <<EOF
 const { getInput, fromRaw } = require('./utils');
 const input = getInput(${1});
 const sample = fromRaw(\`\`);
